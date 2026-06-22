8bd84d036ebfc7a8c42bb2f03505d02fdc03a81a
msaleemzp <mo@zeropath.com>
2026-06-22 14:20:11 -0400

    add fetch

--- index.js
+++ index.js
@@ -18,3 +18,42 @@ function isPrivateIp(ip) {
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+
+app.get('/fetch', async (req, res) => {
+    const url = req.query.url;
+    let parsedUrl;
+    try {
+      parsedUrl = new URL(url);
+    } catch (e) {
+      return res.status(400).send('Invalid URL');
+    }
+    const hostname = parsedUrl.hostname;
+    if (!['http:', 'https:'].includes(parsedUrl.protocol) ||
+        hostname === 'localhost' ||
+        hostname === '127.0.0.1' ||
+        hostname === '::1' ||
+        /^(10|127)\./.test(hostname) ||
+        /^172\.(1[6-9]|2[0-9]|3[0-1])\./.test(hostname) ||
+        /^192\.168\./.test(hostname)) {
+      return res.status(400).send('URL not allowed');
+    }
+    try {
+    // DNS resolution to prevent DNS rebinding
+    try {
+      const addresses = await dns.lookup(parsedUrl.hostname, { all: true });
+      for (const { address } of addresses) {
+        if (isPrivateIp(address)) {
+          return res.status(400).send('URL not allowed');
+        }
+      }
+    } catch (e) {
+      return res.status(400).send('Invalid hostname');
+    }
+  
+      const resp = await axios.get(url);
+      res.send(resp.data);
+    } catch (e) {
+      res.status(500).send(e.message);
+    }
+  });
